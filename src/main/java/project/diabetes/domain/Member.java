4614a51e98518907c0baef3824b59c952fca9a06
USER <cji513@naver.com>
2023-09-03 17:39:06 +0900

    commit

--- src/main/java/project/diabetes/domain/Member.java
+++ src/main/java/project/diabetes/domain/Member.java
@@ -21,7 +21,7 @@ public class Member {
     private Integer icr;
     private float height;
     private float weight;
-    private float goal;
+    private Integer goal;
 
     private String userId;
 }
