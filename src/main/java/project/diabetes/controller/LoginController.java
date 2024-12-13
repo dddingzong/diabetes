ee54ce35a5774971b5dcb61ef3de203d0e3aaba3
USER <cji513@naver.com>
2024-12-13 22:42:53 +0900

    refactor(LoginController): 정렬

--- src/main/java/project/diabetes/controller/LoginController.java
+++ src/main/java/project/diabetes/controller/LoginController.java
@@ -1,8 +1,6 @@
 package project.diabetes.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.NonUniqueResultException;
-import org.hibernate.QueryException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +9,6 @@ import project.diabetes.domain.Login;
 import project.diabetes.domain.Member;
 import project.diabetes.service.LoginService;
 
-import javax.persistence.NoResultException;
-
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
@@ -20,37 +16,37 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/") //로그인 페이지
-    public String home(){
+    public String home() {
         return "logIn";
     }
 
     @PostMapping("/")
-    public String signUp(Model model, String userId, String userEmail, String userPassword, String userPassword2){
+    public String signUp(Model model, String userId, String userEmail, String userPassword, String userPassword2) {
 
         // 아이디가 중복된 경우 (true 면 중복)
-        if (loginService.checkDuplicateId(userId)){
-            model.addAttribute("warning","아이디가 중복되었습니다.");
+        if (loginService.checkDuplicateId(userId)) {
+            model.addAttribute("warning", "아이디가 중복되었습니다.");
             return "logIn";
         }
 
         //비밀번호와 비밀번호 확인이 다른경우
-        if (!(userPassword.equals(userPassword2))){
-            model.addAttribute("warning","비밀번호와 비밀번호 재입력이 다릅니다.");
+        if (!(userPassword.equals(userPassword2))) {
+            model.addAttribute("warning", "비밀번호와 비밀번호 재입력이 다릅니다.");
             return "logIn";
         }
 
-        Login user = new Login(userId,userEmail,userPassword);
+        Login user = new Login(userId, userEmail, userPassword);
         loginService.signUp(user);
 
         return "logIn";
     }
 
     @PostMapping("/real_login")
-    public String logIn(Model model,String login_userId, String login_userPassword){
+    public String logIn(Model model, String login_userId, String login_userPassword) {
 
         // 아이디가 존재하지 않는 경우 (true면 존재)
-        if (!(loginService.checkNoUserId(login_userId))){
-            model.addAttribute("warning","아이디가 존재하지 않습니다.");
+        if (!(loginService.checkNoUserId(login_userId))) {
+            model.addAttribute("warning", "아이디가 존재하지 않습니다.");
             return "logIn";
         }
 
@@ -58,17 +54,17 @@ public class LoginController {
         Login logincheck = loginService.login(login_userId);
 
         // 그에 맞는 userPassword 검색 (틀리면 없다고 경고문)
-        if (!(login_userPassword.equals(logincheck.getUserPassword()))){
-            model.addAttribute("warning","비밀번호가 틀렸습니다.");
+        if (!(login_userPassword.equals(logincheck.getUserPassword()))) {
+            model.addAttribute("warning", "비밀번호가 틀렸습니다.");
             return "logIn";
         }
 
         // uesrId를 사용해 Member table 에서 member_id를 가져옴 (이제 모든 DB의 구분자)
         Member member = loginService.findMember(login_userId);
         Long member_id = member.getId();
-        model.addAttribute("memberId",member_id);
+        model.addAttribute("memberId", member_id);
 
         String originalUrl = "redirect:/info/";
-        return originalUrl+member_id;
+        return originalUrl + member_id;
     }
 }
