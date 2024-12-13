be7d74709d3961939825834220f8e0181a00ca62
USER <cji513@naver.com>
2024-12-13 22:58:44 +0900

    refactor(MainController): 정렬

--- src/main/java/project/diabetes/controller/MainController.java
+++ src/main/java/project/diabetes/controller/MainController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping("/first") // 처음 로그인 데이터 받기
-    public String about(){
+    public String about() {
         return "first";
     }
+
     @GetMapping("/record") //혈당등록 페이지
-    public String record(){
+    public String record() {
         return "record";
     }
 }
